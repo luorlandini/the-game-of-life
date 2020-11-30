fbb5e111424402aac6ba14bfe0216e13aa1ccf00
Luca Orlandini <luca.orlandini@docomodigital.com>
2020-11-30 22:05:50 +0100

    add stop and random

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React from 'react';
 import Grid from '../Grid';
 import { Button } from 'react-bootstrap';
 import { nextGeneration, createBoard } from '../../lib/life';
 import { BOARD_SIZE, DEAD, PLAY_TIME } from '../../constants'
+import { randomize } from '../../utilities'
 
 class Board extends React.Component {
 
-    state = {
-        community: createBoard(BOARD_SIZE, DEAD),
-        generation: 0,
-        playing: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            community: createBoard(BOARD_SIZE, DEAD),
+            generation: 0,
+            playing: false,
+        }
     }
 
     changeState = (community, generation) => this.setState({
@@ -18,12 +22,23 @@ class Board extends React.Component {
     });
 
     onChangeGrid = (comm) => { this.changeState(comm, this.state.generation + 1) }
+
     nextGeneretion = () => {
         this.onChangeGrid(nextGeneration(this.state.community));
     }
     run = () => {
-        let intervalId = setInterval(() => this.nextGeneretion(), PLAY_TIME);
+        this.intervalId = setInterval(() => this.nextGeneretion(), PLAY_TIME);
     }
+
+    stop = () => {
+        this.setState({ playing: false });
+        clearInterval(this.intervalId);
+    }
+
+    random = () => {
+        this.changeState(randomize(this.state.community), 0);
+    }
+
     render() {
         const { community, playing } = this.state;
         return (
@@ -31,7 +46,9 @@ class Board extends React.Component {
                 <Grid data={community} changeDataGrid={this.onChangeGrid} />
                 <Button variant="primary" onClick={this.run}>Play</Button>
                 <Button variant="primary" onClick={this.nextGeneretion} >Next</Button>
-            </div >
+                <Button variant="primary" onClick={this.stop} >Stop</Button>
+                <Button variant="primary" onClick={this.random} >Random</Button>
+            </div>
         )
     }
 
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,8 +2,6 @@ export const changeArrayValue = (arr, i, val) => [
     ...arr.slice(0, i), val, ...arr.slice(i + 1)
 ];
 
-export const shuffle = world => world.map(row => row.map(cell => Math.round(Math.random())));
-
 export const _2DArray = (size, value) => {
     return Array(size).fill().map(() => Array(size).fill(value));
 }
