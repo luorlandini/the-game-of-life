26dd140fae27a6ccaccb52727ee0f55012468f76
Luca Orlandini <luca.orlandini@docomodigital.com>
2020-11-30 21:15:35 +0100

    remove cra comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Grid from './components/Grid';
+import Board from './components/Board';
 
 function App() {
 
-  const ClickOnCell = () => {
-
-    console.log('clickOnCell');
-    return true;
-
-  }
-
-  var [r, c] = [50, 50];
-  var m = Array(r).fill().map(() => Array(c).fill(0));
-
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <Grid data={m} />
+      <Board />
     </div>
   );
 }
