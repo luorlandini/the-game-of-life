c299df278393b068c6b714b62aedc7c76a321fb8
Luca Orlandini <luca.orlandini@docomodigital.com>
2020-12-01 17:41:21 +0100

    change const value

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-export const BOARD_SIZE = 10;
+export const BOARD_SIZE = 70;
 export const PLAY_TIME = 1000;
 export const LIVE = 1;
 export const MAX_LIVE = 3;
-export const MIN_LIVE = 3;
+export const MIN_LIVE = 2;
 
 export const DEAD = 0;
